c042405905b39f9706cdb8042b57a02ae7f1bdef
Alex Castillo <acgrocks@hotmail.com>
2022-06-10 12:49:25 +0200

    feed

--- config/initializers/koala.rb
+++
@@ -1,8 +0,0 @@
-Koala.configure do |config|
-  config.access_token = EAAebMOZCpFS0BAD97lZAPQzu5rp8bfAXAS0vNdNZBPoEFDpZCsFa06eBT0erdZCVtIQqj50b6lf8IKZBoW8IflwjRWENhKJB3zZBjOF5N9ScFeZBIZBMGWVLj6sy9P3VS2s3Vak5VRCZAQ2axMBJbZBqhGebPYFFaxj2UVK5biCxTpukcXurVs37kFLiIZBPaW2LIAHqBGCk0o6rZACayo4CdMiMbnALVunBekFMZD
-  config.app_access_token = MY_APP_ACCESS_TOKEN
-  config.app_id = MY_APP_ID
-  config.app_secret = MY_APP_SECRET
-  # See Koala::Configuration for more options, including details on how to send requests through
-  # your own proxy servers.
-end
