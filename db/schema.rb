6aba72af6527d7f2b5374f07e06c7b0687e0c953
Alex Castillo <acgrocks@hotmail.com>
2022-06-14 12:57:44 +0200

    feed4

--- db/schema.rb
+++ db/schema.rb
@@ -96,6 +96,15 @@ ActiveRecord::Schema.define(version: 2022_06_13_152730) do
     t.index ["channel_id"], name: "index_posts_on_channel_id"
   end
 
+  create_table "rewards", force: :cascade do |t|
+    t.text "name"
+    t.integer "price"
+    t.bigint "channel_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["channel_id"], name: "index_rewards_on_channel_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -123,4 +132,5 @@ ActiveRecord::Schema.define(version: 2022_06_13_152730) do
   add_foreign_key "messages", "channels"
   add_foreign_key "messages", "users"
   add_foreign_key "posts", "channels"
+  add_foreign_key "rewards", "channels"
 end
